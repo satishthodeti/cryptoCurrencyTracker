ef2c7dc39d20471c583e83ce7f213a569226dd9f
satishthodeti <satishthodeti39gamail.com>
2023-06-13 02:12:06 +0000

    Solution Code

---
+++ src/components/CryptocurrencyTracker/index.js
@@ -0,0 +1,9 @@
+import CryptocurrenciesList from '../CryptocurrenciesList'
+import './index.css'
+
+const CryptocurrencyTracker = () => (
+  <div className="crypto-black-container">
+    <CryptocurrenciesList />
+  </div>
+)
+export default CryptocurrencyTracker
